a185dd529bb3c497cd737def5bf6627efb79266a
Robin Devitt <iamrobindevitt@gmail.com>
2025-05-26 14:43:52 +0200

    add a filter to set records per page

--- classes/class-wll-login-records.php
+++ classes/class-wll-login-records.php
@@ -98,8 +98,7 @@ class WLL_Records extends WP_List_Table {
 
 		$this->_column_headers = [ $columns, $hidden, $sortable ];
 
-		// Pagination variables
-		$per_page     = 10;
+		$per_page     = apply_filters( 'wll_recorders_per_page', 20 );
 		$current_page = $this->get_pagenum();
 		$offset       = ( $current_page - 1 ) * $per_page;
 
