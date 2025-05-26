8ca32eefa12d58a8476f16b8f5cf01356495b6a4
Robin Devitt <iamrobindevitt@gmail.com>
2025-05-26 14:24:28 +0200

    plugin functionality

--- when-last-login.php
+++ when-last-login.php
@@ -22,6 +22,9 @@ require 'vendor/autoload.php';
 register_activation_hook( __FILE__, 'wll_activate' );
 register_deactivation_hook( __FILE__, 'wll_deactivate' );
 
+add_filter( 'plugin_row_meta', 'wll_plugin_row_meta', 10, 2 );
+add_filter( 'plugin_action_links_' . WLL_BASENAME, 'wll_plugin_action_links', 10, 2 );
+
 add_filter( 'cron_schedules', function ( $schedules ) {
 	$schedules['wll_migration_schedule'] = [
 		'interval' => apply_filters( 'wll_migration_interval', 300 ),
@@ -31,7 +34,7 @@ add_filter( 'cron_schedules', function ( $schedules ) {
 } );
 
 /**
- * Plugin activation
+ * Plugin activation.
  */
 function wll_activate() {
 	WLL_Database::create_table();
@@ -42,13 +45,57 @@ function wll_activate() {
 }
 
 /**
- * Plugin deactivation
+ * Plugin deactivation.
  */
 function wll_deactivate() {
 	wp_clear_scheduled_hook( 'wll_migrate_login_records_event' );
 }
 
+/**
+ * Add meta links to the plugin row.
+ *
+ * @param array  $links The existing links.
+ * @param string $file The plugin file.
+ * @return array
+ */
+function wll_plugin_row_meta( $links, $file ) {
+	if ( strpos( $file, 'when-last-login.php' ) !== false ) {
+		$new_links = array(
+		'<a href="' . admin_url('admin.php?page=when-last-login-settings') . '" title="' . esc_attr( __( 'View Settings', 'when-last-login' ) ) . '">' . __( 'Settings', 'when-last-login' ) . '</a>',
+		'<a href="' . esc_url( 'https://yoohooplugins.com/?s=when+last+login' ) . '" title="' . esc_attr__( 'View Documentation', 'when-last-login' ) . '">' . esc_html__( 'Docs', 'when-last-login' ) . '</a>',
+		'<a href="' . esc_url( 'https://yoohooplugins.com/support/' ) . '" title="' . esc_attr__( 'Visit Customer Support Forum', 'when-last-login' ) . '">' . esc_html__( 'Support', 'when-last-login' ) . '</a>',
+		);
+
+		$new_links = apply_filters( 'wll_plugin_row_meta', $new_links );
+		$links = array_merge( $links, $new_links );
+	}
+	return $links;
+}
+
+/**
+ * Add action links to the plugin.
+ *
+ * @param array $links The existing links.
+ * @return array
+ */
+function wll_plugin_action_links( $links ) {
+	$new_links = array(
+		'<a href="' . admin_url('admin.php?page=when-last-login-settings') . '" title="' . esc_attr( __( 'View Settings', 'when-last-login' ) ) . '">' . __( 'Settings', 'when-last-login' ) . '</a>'
+	);
+
+	$new_links = apply_filters( 'wll_plugin_action_links', $new_links );
+
+	return array_merge( $new_links, $links );
+}
 
 new WLL_Database();
 When_Last_Login::get_instance();
 new WLL_Admin();
+new WLL_User_Registration_Login();
+new WLL_Widgets();
+new WLL_Columns();
+
+// Load PMPRO integration if PMPRO is active.
+if ( defined( 'PMPRO_VERSION' ) ) {
+	new WLL_PMPRO();
+}
